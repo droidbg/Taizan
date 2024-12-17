08935babea66e348daa2728a2a7905b7ce5efbe3
Binni G <41873024+droidbg@users.noreply.github.com>
2024-12-17 17:57:54 +0530

    add. content heading

--- src/pages/BestSeller.js
+++ src/pages/BestSeller.js
@@ -1,14 +1,15 @@
 import Card from "../components/Card";
 
 const BestSeller = () => {
+  const item = "Electronics";
   return (
     <div>
       <div className="bestseller-heading">
         <h1>Taigan Bestsellers</h1>
         <h4> Our most popular products based on sales. Updated frequently</h4>
       </div>
-      <div className="bestseller-body">
-        <h3>Bestsellers in Electronics</h3>
+      <div className="bestseller-content">
+        <h1>Bestsellers in {item}</h1>
         <div className="products">
           <ul>
             <Card />
