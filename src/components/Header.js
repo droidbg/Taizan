c6ad5ebfaca9c1242286f10c912418b94abf5182
Binni G <41873024+droidbg@users.noreply.github.com>
2024-12-17 17:30:21 +0530

    add. items and style in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,27 @@
+import logo from "../../public/images/taigan.png";
+
 const Header = () => {
-  return <div className="header">Header</div>;
+  const name = "Taizan Users";
+  return (
+    <div className="header">
+      <img className="logo-header" src={logo} />
+      <div className="header-middle">
+        <ul>
+          <li className="header-item">Electronics</li>
+          <li className="header-item">Mobile</li>
+          <li className="header-item">Laptop</li>
+          <li className="header-item">TV</li>
+        </ul>
+      </div>
+      <div className="header-end">
+        <ul>
+          <li className="header-item">Hello, {name}</li>
+          <li className="header-item">Order</li>
+          <li className="header-item">Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
